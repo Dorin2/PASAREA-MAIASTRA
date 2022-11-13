5d358ee67a769f109e12ce51de6f353e12dbd374
Dorin <dorin.ghetus98@gmail.com>
2022-11-13 16:00:50 +0200

    C4

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = ()=>{
+    return (
+        <>
+        <img  className ="background" src="pasare22.jpg" alt="background"/>
+        <div>dorin
+
+        </div>
+
+        </>
+    )
+
+}
+
+export default Header;
+
+
+
