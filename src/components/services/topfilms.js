dd2b9ba0c457f34d7aae9a3b427d9046880ddb5c
Dorin <dorin.ghetus98@gmail.com>
2022-11-17 21:33:50 +0200

    c12

--- src/components/services/topfilms.js
+++ src/components/services/topfilms.js
@@ -5,21 +5,25 @@ import React from "react";
 
 const Doptions = ()=>{
 
-    const paul = fetch('https://imdb8.p.rapidapi.com/title/v2/find?title=The%20Dark%20Knight%20(2008)&limit=20&sortArg=moviemeter%2Casc')
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    const paul = () =>{
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'f098b1f8eemsha2e550004965d05p164e7ejsn9c8baeeb8d86',
+                'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+            }
+        };
+        
+        fetch('https://imdb8.p.rapidapi.com/title/'v2/find?title=Pirates%20of%20the%20Caribbean%3A%20Dead%20Mans%20Chest&limit=1&sortArg=moviemeter%2Casc', options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    };
 
-
+    
 return(
-
-    
-    <>
-    <paul/>
-
-
-    
-    </>
+    paul()
+   
 )
 
 }
